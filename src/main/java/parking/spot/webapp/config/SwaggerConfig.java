401771c4e9f0f5585c3eb05f0d14727458eea690
patrykks <pskalsk@student.agh.edu.pl>
2017-12-14 11:04:49 +0100

    Changes in swagger config

--- src/main/java/parking/spot/webapp/config/SwaggerConfig.java
+++ src/main/java/parking/spot/webapp/config/SwaggerConfig.java
@@ -1,12 +1,15 @@
 package parking.spot.webapp.config;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -15,7 +18,13 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(paths())
                 .build();
+    }
+
+    private Predicate<String> paths() {
+        return or(
+                regex("/health.*"),
+                regex("/info.*"));
     }
 }
